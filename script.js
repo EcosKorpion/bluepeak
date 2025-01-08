d36ce36b90c05ff122582ef84413093ca3048426
EcosKorpion <gabriel86alencar@gmail.com>
2025-01-08 18:28:15 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let GetStarted=document.querySelector('.div-wrapper-3')
-let Header=document.querySelector('.header')
-window.addEventListener('scroll',()=>{let scrollPosition=window.scrollY;if(scrollPosition>=420){Header.style.position='absolute';Header.style.top='420px';console.log('Header fixo na posição:',scrollPosition);}else{Header.style.position='fixed';Header.style.top='0px';}
-console.log('Posição do scroll (em pixels):',scrollPosition);});GetStarted.onclick=()=>{console.log('Get Started button clicked');}
